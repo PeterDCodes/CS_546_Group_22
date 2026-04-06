7e9a5c17b443361842de9f860d95015a9885781f
Olivia Longmuir <olivia.l.longmuir@icloud.com>
2026-04-05 20:34:09 -0400

    initial push of routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,12 @@
 // INDEX FOR ROUTES
+import {static as staticDir} from 'express';
+
+const constructorMethod = (app) => {
+    app.use('/', mealRoutes);
+    app.use('/public', staticDir('public'));
+    app.use(/(.*)/, (req, res) => {
+        res.status(404).render('error');
+    });
+};
+
+export default constructorMethod;
